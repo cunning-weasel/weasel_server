760aeb6720b3db1abddb45c097e1e4d848101e87
weasel <paulbongers22@gmail.com>
2023-10-13 21:22:09 +0200

    add updated folio

---
+++ index/blog/2023/Sep/Growth.md
@@ -0,0 +1,25 @@
+## Why not: Jack of all trades, master of one, or two, or three, maybe four? Maybe more?
+
+I'm not sure where this mentality came from, or who has been pushing its narrative over the years, but want to address this once and for all in the article.
+
+Since time in memorium, the idea that someone, or something tends to be a "jack of all trades, master or none" has been the mentality for years. And we often just accept it, like life really is that binary.
+
+I can remember the first instance I experienced something along those lines, at least professionally. I studied tourism management and consultancy. Unlike every other student at the time, I opted to go into hotels as a first stop in the working-world of adulthood.
+What surprised me is how many people were shocked by this.
+"Wait, you studied tourism, **_and don't_** want to work for an airline or a travel agent!?"
+
+Since the last 2 years of being a freelancer, it's a box I often peer into, as I take on new contracts and clients, get added into team's rotations, or simply get to watch from the free-~~lancer~~lines.
+
+What I've come to learn over the years is that the (mis?)-labelling and misrepresentation of generalists vs specialists comes down to one thing and one thing alone.
+
+It comes down to management. Not being able to utilize people who 'get shit done' is a strategic error, while not being able to exploit people's talents who look at the broader picture is a tactical one. 
+
+Warriors going small-ball to win games? Strategy. Alternate between resting Curry and Klay during a game? Tactics. I myself have issues avoiding the "jack of all trade" trap. I should think the key to one's own growth is to recognize their strengths and be ok with it. And if you tend to have a strong interest in something else and can get paid for it, then you're more diverse. 
+
+To quote Chat Holmes' brilliant book The Ultimate Sales Machine:
+
+>> ... there are three types of executives. A full 90% are what I call 'tactical executives', while 9% are 'strategic'. 
+>
+>> ... tactical executives don't understand strategy. If you tell them that it's twice as hard today as it was 10 years ago to get an appointment with a with a prospect, they will think of ways to make the sales team work twice as hard. 
+>
+>> ... and only 1% - the most effective executives - possess the rare combintation of both tactical and strategic abilities.
--- index/script.js
+++ index/script.js
@@ -1,100 +1,181 @@
 "use strict";
-// change up for server endpoints
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
+  const articleList = document.querySelector("#blogArticles");
+  const blogArticleContent = document.querySelector("#blogArticleContent");
   const searchInput = document.querySelector(".searchBox");
-  searchInput.disabled = true;
-  let repoList = document.querySelector("#repos");
-  let repos = [];
+  // let articles = [];
+  const articlePaths = [
+    "/blog/2023/Sep/Growth.md",
+    "/blog/2023/Aug/Anime.md",
+    "/blog/2023/Jun/Pico.md",
+    //
+  ];
+  const monthsToRetrieve = ["Jun", "Aug", "Sep"];
+  // searchInput.disabled = true;
 
-  searchInput.addEventListener("keyup", (e) => {
-    const searchStr = e.target.value.toLowerCase();
-    const filteredRepos = repos.filter((repo) => {
-      return (
-        repo.name.toLowerCase().includes(searchStr) ||
-        (repo.language && repo.language.toLowerCase().includes(searchStr)) ||
-        (repo.description && repo.description.toLowerCase().includes(searchStr))
-      );
-    });
-    showRepos(filteredRepos);
-  });
-
-  const pullRepos = async () => {
+  const fetchMarkdownFile = async (articlePath) => {
     try {
-      const api_url = "https://api.github.com/users/cunning-weasel/repos";
-      const res = await fetch(api_url);
-      repos = await res.json();
-      showRepos(repos);
-      searchInput.disabled = false;
-    } catch (err) {
-      console.error(err);
+      const resp = await fetch(articlePath);
+      if (resp.ok) {
+        const markdownText = await resp.text();
+        // console.log("markdownText fetch:", markdownText);
+        return markdownText;
+      } else {
+        throw new Error("Failed to fetch .md file");
+      }
+    } catch (error) {
+      console.error(error);
+      return null;
     }
   };
 
-  const showRepos = (repos) => {
-    const htmlString = repos
-      .map((repo) => {
-        return `
-        <div class="card">
-            <div class="card-container">
+  const showArticles = async (articlePaths) => {
+    const htmlContent = await Promise.all(
+      articlePaths
+        .filter((path) => {
+          const month = path.split("/")[3];
+          return monthsToRetrieve.includes(month);
+        })
+        .map(async (path, index) => {
+          const markdownText = await fetchMarkdownFile(path);
+          // console.log("path:", path, index);
+          const month = path.split("/")[3];
+          // const title = path.split("/")[4].replace(/\.md$/, '');
+          const content = marked.parse(markdownText);
+          const quickContent = content.split(".")[0];
+          // console.log("content:", content);
+
+          return `
+            <div class="card">
+              <div class="card-container">
                 <div>
-                    <p>${repo.language}</p>
+                  <p>${month}</p>
                 </div>
-                <h2>${repo.name}</h2>
+              </div>
+              <div>
+                <p>${quickContent}...</p>
+              </div>
+              <div>
+              <a href="blogArticle.html?articlePath=${encodeURIComponent(path)}" class="btn read-more-link">Read more</a>
+              </div>
             </div>
-                <div>
-                    <p>${repo.description}</p>
-                </div>
-                <div>
-                    <a href="${repo.homepage}" class="btn">Read more</a>
-                </div>
-        </div>
           `;
-      })
-      .join("");
-    repoList.innerHTML = htmlString;
+        })
+    );
+    articleList.innerHTML = htmlContent.join("");
   };
-  pullRepos();
+
+  // articlePath query param
+  const urlParams = new URLSearchParams(window.location.search);
+  const articlePath = urlParams.get("articlePath");
+
+  // load content based on articlePath
+  if (articlePath) {
+    const markdownText = await fetchMarkdownFile(decodeURIComponent(articlePath));
+    const htmlContent = marked.parse(markdownText);
+    blogArticleContent.innerHTML = htmlContent;
+  }
+
+  // initial render
+  showArticles(articlePaths);
+
+  // search render
+  searchInput.disabled = false;
+  searchInput.addEventListener("input", (e) => {
+    const searchStr = e.target.value.toLowerCase();
+    const cards = articleList.querySelectorAll('.card');
+    cards.forEach((card) => {
+      const cardContent = card.querySelector('h2').textContent.toLowerCase();
+      if (cardContent.includes(searchStr)) {
+        card.style.display = 'block';
+      } else {
+        card.style.display = 'none';
+      }
+    });
+  });
 });
 
-// begin file read ish
-
-
-// async function* getFilesRecursively(entry) {
-//   if (entry.kind === "file") {
-//     const file = await entry.getFile();
-//     if (file !== null) {
-//       file.relativePath = getRelativePath(entry);
-//       yield file;
-//     }
-//   } else if (entry.kind === "directory") {
-//     for await (const handle of entry.values()) {
-//       yield* getFilesRecursively(handle);
-//     }
-//   }
-// }
-// for await (const fileHandle of getFilesRecursively(directoryHandle)) {
-//   console.log(fileHandle);
-// }
-
-// getFilesRecursively("blog");
-
-
 // needs to be publish date - some static var
-const currentDate = new Date(Date.now());
+// const currentDate = new Date(Date.now());
 
-const dateFormat = {
-  year: "numeric",
-  month: "long",
-  day: "numeric",
-};
+// const dateFormat = {
+//   year: "numeric",
+//   month: "long",
+//   day: "numeric",
+// };
 
-const formattedDate = new Intl.DateTimeFormat("en-US", dateFormat).format(
-  currentDate
-);
+// const formattedDate = new Intl.DateTimeFormat("en-US", dateFormat).format(
+//   currentDate
+// );
 
-document.getElementById("currentDate").textContent = formattedDate;
+// document.getElementById("currentDate").textContent = formattedDate;
 
-// pass endpoints from above
-document.getElementById("content").innerHTML = marked.parse(
-  "# Article title goes here\n\nRest of the text and some **bold** sample all written in .md and rendered to html with marked."
-);
+
+
+
+
+
+
+
+
+/*
+
+old site
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const searchInput = document.querySelector(".searchBox");
+//   searchInput.disabled = true;
+//   let repoList = document.querySelector("#repos");
+//   let repos = [];
+
+//   searchInput.addEventListener("keyup", (e) => {
+//     const searchStr = e.target.value.toLowerCase();
+//     const filteredRepos = repos.filter((repo) => {
+//       return (
+//         repo.name.toLowerCase().includes(searchStr) ||
+//         (repo.language && repo.language.toLowerCase().includes(searchStr)) ||
+//         (repo.description && repo.description.toLowerCase().includes(searchStr))
+//       );
+//     });
+//     showRepos(filteredRepos);
+//   });
+
+//   const pullRepos = async () => {
+//     try {
+//       const api_url = "https://api.github.com/users/cunning-weasel/repos";
+//       const res = await fetch(api_url);
+//       repos = await res.json();
+//       showRepos(repos);
+//       searchInput.disabled = false;
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   };
+
+//   const showRepos = (repos) => {
+//     const htmlString = repos
+//       .map((repo) => {
+//         return `
+//         <div class="card">
+//             <div class="card-container">
+//                 <div>
+//                     <p>${repo.language}</p>
+//                 </div>
+//                 <h2>${repo.name}</h2>
+//             </div>
+//                 <div>
+//                     <p>${repo.description}</p>
+//                 </div>
+//                 <div>
+//                     <a href="${repo.homepage}" class="btn">Read more</a>
+//                 </div>
+//         </div>
+//           `;
+//       })
+//       .join("");
+//     repoList.innerHTML = htmlString;
+//   };
+//   pullRepos();
+// });
+*/
