109db717892f1d2b793aa8ad12a5430cdce4ab0e
weasel <paulbongers22@gmail.com>
2024-06-16 08:39:22 +0200

    x

--- build.sh
+++ build.sh
@@ -5,20 +5,22 @@ echo "[2] Debug 64-bit"
 echo "[3] Regular compile"
 echo "[4] Optimized compile"
 read -p "Enter your cunning choice: " choice
+
+compile_and_execute() {
+    gcc -o output_weasel_server weasel_server.c "$@"
+    if [ $? -eq 0 ]; then
+        ./output_weasel_server
+    else
+        echo "Compilation failed."
+    fi
+}
+
 case $choice in
-    1)
-        -Wall -Wextra -Wpedantic -Og -g3 -fsanitize=address,undefined
-        ;;
-    2)
-        -Wall -Wextra -Wpedantic -Og -g3 -fsanitize=address,undefined -m64
-        ;;
-    3)
-        gcc -o output_weasel_server weasel_server.c
-        ;;
-    4)
-        gcc -o output_hm_weasel hm_weasel.c -O3
-        ;;
-    *)
-        echo "Invalid option"
-        ;;
+    1) gcc -o output_weasel_server weasel_server.c -g -Og -Wall -Wextra -Wpedantic -Og -g3 -fsanitize=address,undefined ;;
+    2) gcc -o output_weasel_server weasel_server.c -g -Og -Wall -Wextra -Wpedantic -Og -g3 -fsanitize=address,undefined -m64 ;;
+    3) gcc -o output_weasel_server weasel_server.c ;;
+    4) gcc -o output_weasel_server weasel_server.c -O3 ;;
+    *) echo "Invalid option" ;;
 esac
+
+compile_and_execute
